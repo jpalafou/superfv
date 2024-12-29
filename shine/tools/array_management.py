4b8e2c27300b130b70339e079d4cfb3195dcbd6e
Jonathan Palafoutas <jpalafou@princeton.edu>
2024-12-29 16:00:26 -0600

    add: ode solver, timer, array manager

---
+++ shine/tools/array_management.py
@@ -0,0 +1,152 @@
+import numpy as np
+from typing import Dict, List, Union
+import warnings
+
+try:
+    import cupy as cp
+
+    CUPY_AVAILABLE = True
+    ArrayType = Union[np.ndarray, cp.ndarray] 
+except Exception:
+    CUPY_AVAILABLE = False
+    ArrayType = np.ndarray
+
+class ArrayManager:
+    """
+    Class for managing arrays in a dictionary and their transfer between NumPy and
+    CuPy.
+    """
+
+    def __init__(self, arrays: Dict[str, ArrayType] = None):
+        """
+        Initializes the array manager.
+
+        Args:
+            arrays: Dictionary of arrays.
+        """
+        self.arrays = arrays if arrays else {}
+        self.using_cupy = False
+
+    def __repr__(self) -> str:
+        return f"ArrayManager({self.arrays.keys()})"
+
+    def __str__(self) -> str:
+        return f"ArrayManager({self.arrays.keys()})"
+
+    def _check_name_exists(self, name: str):
+        if name not in self.arrays:
+            raise KeyError(f"Array with name '{name}' not found.")
+
+    def _check_name_available(self, name: str):
+        if name in self.arrays:
+            raise KeyError(f"Array with name '{name}' already exists.")
+
+    def _check_numpy_array(self, array: np.ndarray):
+        if not isinstance(array, np.ndarray):
+            raise TypeError(f"Array must be of type numpy.ndarray, not {type(array)}.")
+
+    def enable_cupy(self):
+        """
+        Enable the use of CuPy.
+
+        Note:
+            If CuPy is available, this method transfers all existing arrays to the
+            GPU as CuPy arrays and sets the `using_cupy` attribute to `True`. Any
+            new arrays added will also be stored as CuPy arrays.
+
+            If CuPy is not available, a warning is printed, and the arrays remain
+            as NumPy arrays on the CPU.
+        """
+        if CUPY_AVAILABLE:
+            self.using_cupy = True
+            for name in self.arrays:
+                self.transfer_device(name, "gpu")
+        else:
+            warnings.warn("CuPy is not available. Falling back to NumPy.")
+
+    def transfer_device(self, name: str, to_device: str):
+        """
+        Transfer an array to a different device.
+
+        Args:
+            name: Name of the array.
+            to_device: Device to transfer the array to. Options are "cpu" and "gpu".
+        """
+        self._check_name_exists(name)
+        if self.using_cupy:
+            if to_device == "cpu" and isinstance(self.arrays[name], cp.ndarray):
+                self.arrays[name] = cp.asnumpy(self.arrays[name])
+            elif to_device == "gpu" and isinstance(self.arrays[name], np.ndarray):
+                self.arrays[name] = cp.asarray(self.arrays[name])
+            else:
+                raise ValueError(f"Invalid device: {to_device}")
+
+    def disable_cupy(self):
+        """
+        Disable the use of CuPy.
+
+        Note:
+            This method transfers all existing arrays to the CPU as NumPy arrays and
+            sets the `using_cupy` attribute to `False`. Any new arrays added will also
+            be stored as NumPy arrays.
+        """
+        for name in self.arrays:
+            self.transver_device(name, "cpu")
+        self.using_cupy = False
+
+    def add(self, name: str, array: np.ndarray):
+        """
+        Add an array to the manager.
+
+        Args:
+            name: Name of the array.
+            array: Array to add.
+        """
+        self._check_name_available(name)
+        self._check_numpy_array(array)
+        if self.using_cupy:
+            self.arrays[name] = cp.asarray(array)
+        else:
+            self.arrays[name] = array
+
+    def rm(self, name: str):
+        """
+        Remove an array from the manager.
+
+        Args:
+            name: Name of the array.
+        """
+        self._check_name_exists(name)
+        del self.arrays[name]
+
+    def clear(self, all_but: List[str] = None):
+        """
+        Clear all arrays from the manager.
+
+        Args:
+            all_but: List of array names to keep.
+        """
+        if all_but:
+            self.arrays = {name: array for name, array in self.arrays.items() if name in all_but}
+        else:
+            self.arrays = {}
+
+    def __call__(self, name: str, asnumpy:bool=False, copy:bool=False) -> ArrayType:
+        """
+        Get an array from the manager.
+
+        Args:
+            name: Name of the array.
+            asnumpy: If True, return the array as a NumPy array.
+            copy: If True, return a copy of the array.
+
+        Returns:
+            Array.
+        """
+        self._check_name_exists(name)
+        if self.using_cupy and asnumpy:
+            return cp.asnumpy(array)
+        return self.arrays[name].copy() if copy else self.arrays[name]
+
+    def to_dict(self) -> dict:
+        return dict(names=list(self.arrays.keys()), using_cupy=self.using_cupy) 
---
+++ shine/tools/timer.py
@@ -0,0 +1,106 @@
+import numpy as np
+import time
+from typing import Tuple
+
+
+class Timer:
+    """
+    Timer class for timing code execution.
+    """
+
+    def __init__(self, cats: Tuple[str] = (), precision: int = 2):
+        """
+        Initializes the timer.
+
+        Args:
+            cats: Tuple of categories.
+            precision: Precision of the time values.
+        """
+        self.cats = set(cats)
+        self.start_time = {}
+        self.cum_time = {}
+
+        self.precision = precision
+
+    def add_cat(self, cat: str):
+        """
+        Add a new timer category.
+
+        Args:
+            cat: Category.
+        """
+        if cat in self.cats:
+            raise ValueError(f"Category '{c}' already exists.")
+        self.cats.add(cat)
+        self.start_time[cat] = None
+        self.cum_time[cat] = 0.0
+
+    def _check_cat_existence(self, cat: str):
+        if cat not in self.cats:
+            raise ValueError(f"Category '{cat}' not found in timer categories.")
+
+    def start(self, cat: str):
+        """
+        Start timer of a category.
+
+        Args:
+            cat: Category.
+        """
+        self.check_cat_existence(cat)
+        if self.start_time[cat] is not None:
+            raise RuntimeError(
+                f"Cannot start '{cat}' timer since it is already in progress."
+            )
+        self.start_time[cat] = time.time()
+
+    def stop(self, cat: str):
+        """
+        Stop timer of a category.
+
+        Args:
+            cat: Category.
+        """
+        self.check_cat_existence(cat)
+        if self._start_time[cat] is None:
+            raise RuntimeError(
+                f"Cannot stop '{cat}' timer since it is not in progress."
+            )
+        self.cum_time[cat] += time.time() - self._start_time[cat]
+        self._start_time[cat] = None
+
+    def to_dict(self) -> dict:
+        out = {cat: np.round(t, self.precision) for cat, t in self.cum_time.items()}
+        return out
+
+    def report(self) -> str:
+        """
+        Return a formatted string report of the cumulative times for all categories
+        with dynamic column width based on both category name length and time values.
+        """
+        # name headers
+        cat_header = "Category"
+        time_header = "Time (s)"
+
+        # Determine the max length of the category names and the time values
+        max_cat_len = (
+            max(len(cat) for cat in self.cats) if self.cats else len(cat_header)
+        )
+        max_time_len = max(
+            (
+                max(len(f"{t:.{self.precision}f}") for t in self.cum_time.values())
+                if self.cum_time
+                else len(time_header)
+            ),
+            len(time_header),
+        )
+
+        # Build the report as a string with dynamically sized columns
+        report_str = f"{cat_header:<{max_cat_len}} {time_header:<{max_time_len}}\n"
+        report_str += "-" * (max_cat_len + max_time_len + 1) + "\n"
+
+        # Add each category and time, formatted to the correct precision and width
+        for cat, t in self.cum_time.items():
+            time_str = f"{t:.{self.precision}f}"
+            report_str += f"{cat:<{max_cat_len}} {time_str:>{max_time_len}}\n"
+
+        return report_str
