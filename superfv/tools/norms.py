298dbd49f649d52c00e65d6078bbe41b718c1fa7
Jonathan Palafoutas <jpalafou@princeton.edu>
2026-01-16 10:52:26 -0500

    fix: norms return a .item() float

--- superfv/tools/norms.py
+++ superfv/tools/norms.py
@@ -7,18 +7,18 @@ def l1_norm(array: ArrayLike) -> float:
     """
     Compute the L1 norm of an array.
     """
-    return np.mean(np.abs(array))
+    return np.mean(np.abs(array)).item()
 
 
 def l2_norm(array: ArrayLike) -> float:
     """
     Compute the L2 norm of an array.
     """
-    return np.sqrt(np.mean(np.square(array)))
+    return np.sqrt(np.mean(np.square(array))).item()
 
 
 def linf_norm(array: ArrayLike) -> float:
     """
     Compute the L-infinity norm of an array.
     """
-    return np.max(np.abs(array))
+    return np.max(np.abs(array)).item()
