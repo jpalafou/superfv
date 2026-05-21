9016aba90389f330fe220f0dacc30b3a22be1b62
Jonathan Palafoutas <jpalafou@princeton.edu>
2026-05-20 20:59:25 -0400

    start compute_flux_jvp kernel

--- superfv/muscl_driver.py
+++ superfv/muscl_driver.py
@@ -1,12 +1,13 @@
 from __future__ import annotations
 
+from functools import lru_cache
 from typing import TYPE_CHECKING, Literal
 
 import numpy as np
 
 from superfv.axes import DIM_TO_AXIS
 
-from .configs import FluxRecipe, FV_SchemeParameters, HydroParameters
+from .configs import FluxRecipe, FV_SchemeParameters
 from .slope_limiting.muscl import (
     MUSCL_SlopeLimiter,
     compute_MUSCL_slopes,
@@ -42,38 +43,125 @@ def _allocate_MUSCL_arrays(sim: HydroSolver):
             arrays.add("_z_slopes_", sim.xp.empty((nvars, _nx_, _ny_, _nz_)))
 
 
-def compute_flux_jvp(
-    q: ArrayLike,
-    vec: ArrayLike,
-    dim: Literal["x", "y", "z"],
+def _compute_flux_jvp_np(
+    w: np.ndarray,
+    vec: np.ndarray,
+    jvp: np.ndarray,
     idx: VariableIndexMap,
-    hydro_params: HydroParameters,
+    dim: Literal["x", "y", "z"],
+    gamma: float,
+    isothermal: bool = False,
+    iso_cs: float = 1.0,
+):
+    iv = idx("v" + dim)
+
+    jvp[idx("rho")] = w[iv] * vec[idx("rho")] + w[idx("rho")] * vec[iv]
+    jvp[idx("vx")] = w[iv] * vec[idx("vx")]
+    jvp[idx("vy")] = w[iv] * vec[idx("vy")]
+    jvp[idx("vz")] = w[iv] * vec[idx("vz")]
+    jvp[iv] += (1 / w[idx("rho")]) * vec[idx("P")]
+    jvp[idx("P")] = (
+        iso_cs**2 * jvp[idx("rho")]
+        if isothermal
+        else gamma * w[idx("P")] * vec[iv] + w[iv] * vec[idx("P")]
+    )
+    if "passives" in idx.group_var_map:
+        jvp[idx("passives")] = w[iv] * vec[idx("passives")] + w[idx("passives")] * vec[iv]
+
+
+@lru_cache(maxsize=None)
+def _make_compute_flux_jvp_kernel(npassives: int):
+    in_params = (
+        "float64 rho, float64 v1, float64 v2, float64 v3, float P, "
+        "float64 vec_rho, float64 vec_v1, float64 vec_v2, float64 vec_v3, float vec_P, "
+        "float64 gamma, bool isothermal, float64 iso_cs, int32 dim"
+    )
+    for i in range(npassives):
+        in_params += f", float64 passives_{i}"
+    for i in range(npassives):
+        in_params += f", float64 vec_passives_{i}"
+
+    out_params = "float64 jvp_rho, float64 jvp_v1, float64 jvp_v2, float64 jvp_v3, float64 jvp_P"
+    for i in range(npassives):
+        out_params += f", float64 jvp_passives_{i}"
+
+    body = """
+    if (dim == 1) {
+        jvp_rho = v1 * vec_rho + rho * vec_v1;
+        jvp_v1 = v1 * vec_rho + rho * vec_v1 + (1 / rho) * vec_P;
+        jvp_v2 = v2 * vec_rho + rho * vec_v2;
+        jvp_v3 = v3 * vec_rho + rho * vec_v3;
+        jvp_P = isothermal ?
+            iso_cs**2 * jvp_rho : gamma * w[idx("P")] * vec[iv] + w[iv] * vec[idx("P")];
+
+    """
+    for i in range(npassives):
+        body += f"    jvp_passives_{i} = v1 * vec_passives_{i} + rho * vec_v1;"
+    body += """
+    } else if (dim == 2) {
+        jvp_rho = v1 * vec_rho + rho * vec_v1;
+        jvp_v1 = v1 * vec_rho + rho * vec_v1 + (1 / rho) * vec_P;
+        jvp_v2 = v2 * vec_rho + rho * vec_v2;
+        jvp_v3 = v3 * vec_rho + rho * vec_v3;
+        jvp_P = isothermal ?
+            iso_cs**2 * jvp_rho : gamma * w[idx("P")] * vec[iv] + w[iv] * vec[idx("P")];
+    """
+    for i in range(npassives):
+        body += f"    jvp_passives_{i} = v2 * vec_passives_{i} + rho * vec_v2;"
+    body += """
+    } else if(dim == 3) {
+        jvp_rho = v1 * vec_rho + rho * vec_v1;
+        jvp_v1 = v1 * vec_rho + rho * vec_v1 + (1 / rho) *	vec_P;
+        jvp_v2 = v2 * vec_rho + rho * vec_v2;
+        jvp_v3 = v3 * vec_rho + rho * vec_v3;
+        jvp_P = isothermal ?
+            iso_cs**2*jvp_rho : gamma*w[idx("P")]*vec[iv] + w[iv]*vec[idx("P")];
+    }
+    """
+    for i in range(npassives):
+        body += f"    jvp_passives_{i} = v3 * vec_passives_{i} + rho * vec_v3;"
+
+    return cp.ElementwiseKernel(in_params, out_params, body, name="compute_flux_jvp")
+
+
+def compute_flux_jvp(
+    w: ArrayLike,
+    vec: ArrayLike,
+    jvp: ArrayLike,
+    idx: VariableIndexMap,
+    dim: Literal["x", "y", "z"],
+    gamma: float,
+    isothermal: bool = False,
+    iso_cs: float = 1.0,
     primitives: bool = True,
-) -> ArrayLike:
-    """
-    Compute the Jacobian-vector product for the flux function.
-    """
+):
     if not primitives:
         raise NotImplementedError("JVP for conservative variable fluxes not implemented yet.")
 
-    jvp = cp.zeros_like(q) if CUPY_AVAILABLE and isinstance(q, cp.ndarray) else np.zeros_like(q)
-
-    iv = idx("v" + dim)
-
-    jvp[idx("rho")] = q[iv] * vec[idx("rho")] + q[idx("rho")] * vec[iv]
-    jvp[idx("vx")] = q[iv] * vec[idx("vx")]
-    jvp[idx("vy")] = q[iv] * vec[idx("vy")]
-    jvp[idx("vz")] = q[iv] * vec[idx("vz")]
-    jvp[iv] += (1 / q[idx("rho")]) * vec[idx("P")]
-    jvp[idx("P")] = (
-        hydro_params.iso_cs**2 * jvp[idx("rho")]
-        if hydro_params.isothermal
-        else hydro_params.gamma * q[idx("P")] * vec[iv] + q[iv] * vec[idx("P")]
-    )
-    if "passives" in idx.group_var_map:
-        jvp[idx("passives")] = q[iv] * vec[idx("passives")] + q[idx("passives")] * vec[iv]
-
-    return jvp
+    if CUPY_AVAILABLE and isinstance(w, cp.ndarray):
+        raise NotImplementedError("JVP kernel generation not implemented yet.")
+        npassives = len(idx.group_var_map.get("passives", []))
+        kernel = _make_compute_flux_jvp_kernel(npassives)
+        kernel(
+            w[idx("rho")],
+            w[idx("vx")],
+            w[idx("vy")],
+            w[idx("vz")],
+            w[idx("P")],
+            vec[idx("rho")],
+            vec[idx("vx")],
+            vec[idx("vy")],
+            vec[idx("vz")],
+            vec[idx("P")],
+            gamma,
+            isothermal,
+            iso_cs,
+            {"x": 1, "y": 2, "z": 3}[dim],
+            *[w[idx(v)] for v in idx.group_var_map.get("passives", [])],
+            *[vec[idx(v)] for v in idx.group_var_map.get("passives", [])],
+        )
+    else:
+        return _compute_flux_jvp_np(w, vec, jvp, idx, dim, gamma, isothermal, iso_cs)
 
 
 def update_fluxes_with_muscl_scheme(
@@ -137,12 +225,15 @@ def update_fluxes_with_muscl_scheme(
             h = {"x": hx, "y": hy, "z": hz}[dim]
             _slopes_ = _xyz_slopes_[dim]
 
-            _jvp_[...] = compute_flux_jvp(
+            compute_flux_jvp(
                 _q_,
                 _slopes_,
-                dim,
+                _jvp_,
                 idx,
-                hydro_params,
+                dim,
+                gamma=hydro_params.gamma,
+                isothermal=hydro_params.isothermal,
+                iso_cs=hydro_params.iso_cs,
                 primitives=muscl_scheme.flux_recipe != FluxRecipe.CONS_LIM_PRIM,
             )
             _predictor_q_[...] -= 0.5 * _jvp_ * hancock_dt / h
