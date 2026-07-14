5aac95a35125e4d660eaa255de84dc4657934539
Jonathan Palafoutas <jpalafou@princeton.edu>
2026-07-14 11:28:23 -0400

    rm: stubs folder

--- stubs/stencilpal/__init__.pyi
+++
@@ -1,6 +0,0 @@
-from typing import Union
-
-from stencilpal.stencil import Stencil
-
-def conservative_interpolation_stencil(p: int, x: Union[int, float, str]) -> Stencil: ...
-def uniform_quadrature(p: int) -> Stencil: ...
