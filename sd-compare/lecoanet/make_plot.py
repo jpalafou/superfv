2666eef8348044653bacffc58bd0f5750a702d95
Jonathan Palafoutas <jpalafou@princeton.edu>
2026-07-16 09:46:12 -0400

    lecoanet plot includes MUSCL-Hancock result

--- sd-compare/lecoanet/make_plot.py
+++ sd-compare/lecoanet/make_plot.py
@@ -2,18 +2,29 @@ import matplotlib.pyplot as plt
 from script import (
     params_to_dedalus_filename,
     project_dedalus_to_x_y_dye,
+    run_MUSCL_Hancock_sim,
+    run_spd_sim,
     run_superfv_sim,
 )
 
 Re_base10 = 5
 Nref = 4096
 density_jump = 2
-t_sim_approx = 4
+t_sim_approx = 2
 
 NDOF = 2048
 p = 3
 
 dedalus = params_to_dedalus_filename(Re_base10, Nref, density_jump, t_sim_approx)
+
+muscl_hancock_sim = run_MUSCL_Hancock_sim(
+    name="",
+    NDOF=NDOF,
+    Re_base10=Re_base10,
+    Nref=Nref,
+    density_jump=density_jump,
+    t_sim_approx=t_sim_approx,
+)
 
 superfv_sim = run_superfv_sim(
     name="",
@@ -26,16 +37,16 @@ superfv_sim = run_superfv_sim(
     rtol=1e-5,
 )
 
-# spd_sim = run_spd_sim(
-#         name="",
-#         p=p,
-#         NDOF=NDOF,
-#         Re_base10=Re_base10,
-#         Nref=Nref,
-#         density_jump=density_jump,
-#         t_sim_approx=t_sim_approx,
-#         tolerance=1e-5,
-#     )
+spd_sim = run_spd_sim(
+    name="",
+    p=p,
+    NDOF=NDOF,
+    Re_base10=Re_base10,
+    Nref=Nref,
+    density_jump=density_jump,
+    t_sim_approx=t_sim_approx,
+    tolerance=1e-5,
+)
 
 
 def plot_dedalus(ax, filename):
@@ -66,15 +77,18 @@ def plot_sd(ax, sim):
 
 
 if __name__ == "__main__":
-    fig, axs = plt.subplots(1, 3, sharex=True, sharey=True, figsize=(12, 4))
+    fig, axs = plt.subplots(2, 2, sharex=True, sharey=True, figsize=(8, 8))
 
-    axs[0].set_title(f"dedalus (N={Nref})")
-    plot_dedalus(axs[0], dedalus)
+    axs[0, 0].set_title(f"dedalus (N={Nref})")
+    plot_dedalus(axs[0, 0], dedalus)
 
-    axs[1].set_title(f"superfv (p={p}, N={NDOF})")
-    plot_fv(axs[1], superfv_sim)
+    axs[1, 0].set_title(f"MUSCL-Hancock (N={NDOF})")
+    plot_fv(axs[1, 0], muscl_hancock_sim)
 
-    # axs[2].set_title(f"spd (p={p}, N={NDOF})")
-    # plot_sd(axs[2], spd_sim)
+    axs[0, 1].set_title(f"superfv (p={p}, N={NDOF})")
+    plot_fv(axs[0, 1], superfv_sim)
+
+    axs[1, 1].set_title(f"spd (p={p}, N={NDOF})")
+    plot_sd(axs[1, 1], spd_sim)
 
     fig.savefig(f"sd-compare/lecoanet/t={t_sim_approx}.png", dpi=300, bbox_inches="tight")
