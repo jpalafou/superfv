92c36c820b169e91a39fa43bdf1c3e863ed7497c
Jonathan Palafoutas <jpalafou@princeton.edu>
2026-07-18 09:15:21 -0400

    update lecoanet scripts

--- sd-compare/lecoanet/make_plot.py
+++ sd-compare/lecoanet/make_plot.py
@@ -1,3 +1,4 @@
+import cupy as cp
 import matplotlib.pyplot as plt
 from script import (
     params_to_dedalus_filename,
@@ -11,14 +12,13 @@ from script import (
 Re_base10 = 5
 Nref = 4096
 density_jump = 2
-t_sim_approx = 2
+t_sim_approx = 4
 
 NDOF = 2048
-p = 3
 
 dedalus = params_to_dedalus_filename(Re_base10, Nref, density_jump, t_sim_approx)
 
-muscl_hancock_sim = run_MUSCL_Hancock_sim(
+musclhancock = run_MUSCL_Hancock_sim(
     name="",
     NDOF=NDOF,
     Re_base10=Re_base10,
@@ -27,9 +27,9 @@ muscl_hancock_sim = run_MUSCL_Hancock_sim(
     t_sim_approx=t_sim_approx,
 )
 
-superfv_sim = run_superfv_sim(
+superfv4 = run_superfv_sim(
     name="",
-    p=p,
+    p=3,
     NDOF=NDOF,
     Re_base10=Re_base10,
     Nref=Nref,
@@ -38,9 +38,31 @@ superfv_sim = run_superfv_sim(
     rtol=1e-5,
 )
 
-spd_sim = run_spd_sim(
+superfv8 = run_superfv_sim(
     name="",
-    p=p,
+    p=7,
+    NDOF=NDOF,
+    Re_base10=Re_base10,
+    Nref=Nref,
+    density_jump=density_jump,
+    t_sim_approx=t_sim_approx,
+    rtol=1e-5,
+)
+
+spd4 = run_spd_sim(
+    name="",
+    p=3,
+    NDOF=NDOF,
+    Re_base10=Re_base10,
+    Nref=Nref,
+    density_jump=density_jump,
+    t_sim_approx=t_sim_approx,
+    tolerance=1e-5,
+)
+
+spd8 = run_spd_sim(
+    name="",
+    p=7,
     NDOF=NDOF,
     Re_base10=Re_base10,
     Nref=Nref,
@@ -64,7 +86,7 @@ def plot_fv(ax, sim):
 
     x_fv, y_fv, _ = sim.mesh.faces
     z_fv = sim.snapshot_history[-1].w[5, :, :, 0].T
-    return ax.pcolormesh(x_fv, y_fv, z_fv)
+    return ax.pcolormesh(cp.asnumpy(x_fv), cp.asnumpy(y_fv), z_fv)
 
 
 def plot_sd(ax, sim):
@@ -74,22 +96,28 @@ def plot_sd(ax, sim):
     x_sd = sim.regular_faces()[0]
     y_sd = sim.regular_faces()[1]
     z_sd = spd_to_uniform_cell_averaged_dye(sim)
-    return ax.pcolormesh(x_sd, y_sd, z_sd)
+    return ax.pcolormesh(cp.asnumpy(x_sd), cp.asnumpy(y_sd), cp.asnumpy(z_sd))
 
 
 if __name__ == "__main__":
-    fig, axs = plt.subplots(2, 2, sharex=True, sharey=True, figsize=(8, 8))
+    fig, axs = plt.subplots(2, 3, sharex=True, sharey=True, figsize=(12, 8))
 
     axs[0, 0].set_title(f"dedalus (N={Nref})")
     plot_dedalus(axs[0, 0], dedalus)
 
     axs[1, 0].set_title(f"MUSCL-Hancock (N={NDOF})")
-    plot_fv(axs[1, 0], muscl_hancock_sim)
+    plot_fv(axs[1, 0], musclhancock)
 
-    axs[0, 1].set_title(f"superfv (p={p}, N={NDOF})")
-    plot_fv(axs[0, 1], superfv_sim)
+    axs[0, 1].set_title(f"FV4 (N={NDOF})")
+    plot_fv(axs[0, 1], superfv4)
 
-    axs[1, 1].set_title(f"spd (p={p}, N={NDOF})")
-    plot_sd(axs[1, 1], spd_sim)
+    axs[0, 2].set_title(f"FV8 (N={NDOF})")
+    plot_fv(axs[0, 2], superfv8)
+
+    axs[1, 1].set_title(f"SD4 (N={NDOF})")
+    plot_sd(axs[1, 1], spd4)
+
+    axs[1, 2].set_title(f"SD8 (N={NDOF})")
+    plot_sd(axs[1, 2], spd8)
 
     fig.savefig(f"sd-compare/lecoanet/t={t_sim_approx}.png", dpi=300, bbox_inches="tight")
