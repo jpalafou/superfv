c1b9af1c0a1cac57fc84fc7e450b3f9ac457b113
Jonathan Palafoutas <jpalafou@princeton.edu>
2026-07-19 13:03:43 -0400

    run and load multiple outputs

--- sd-compare/lecoanet/make_plot.py
+++ sd-compare/lecoanet/make_plot.py
@@ -7,16 +7,21 @@ from script import (
     run_spd_sim,
     run_superfv_sim,
     spd_to_uniform_cell_averaged_dye,
+    superfv_to_uniform_cell_averaged_dye,
 )
 
 Re_base10 = 5
 Nref = 4096
 density_jump = 2
-t_sim_approx = 4
+target_times = [2, 4, 6]
+nout = 1
+if not 1 <= nout <= len(target_times):
+    raise ValueError(f"nout must be between 1 and {len(target_times)}.")
+t_plot = target_times[nout - 1]
 
 NDOF = 2048
 
-dedalus = params_to_dedalus_filename(Re_base10, Nref, density_jump, t_sim_approx)
+dedalus = params_to_dedalus_filename(Re_base10, Nref, density_jump, t_plot)
 
 musclhancock = run_MUSCL_Hancock_sim(
     name="",
@@ -24,7 +29,7 @@ musclhancock = run_MUSCL_Hancock_sim(
     Re_base10=Re_base10,
     Nref=Nref,
     density_jump=density_jump,
-    t_sim_approx=t_sim_approx,
+    target_times=target_times,
 )
 
 superfv4 = run_superfv_sim(
@@ -34,7 +39,7 @@ superfv4 = run_superfv_sim(
     Re_base10=Re_base10,
     Nref=Nref,
     density_jump=density_jump,
-    t_sim_approx=t_sim_approx,
+    target_times=target_times,
     rtol=1e-5,
 )
 
@@ -45,7 +50,7 @@ superfv8 = run_superfv_sim(
     Re_base10=Re_base10,
     Nref=Nref,
     density_jump=density_jump,
-    t_sim_approx=t_sim_approx,
+    target_times=target_times,
     rtol=1e-5,
 )
 
@@ -56,7 +61,7 @@ spd4 = run_spd_sim(
     Re_base10=Re_base10,
     Nref=Nref,
     density_jump=density_jump,
-    t_sim_approx=t_sim_approx,
+    target_times=target_times,
     tolerance=1e-5,
 )
 
@@ -67,7 +72,7 @@ spd8 = run_spd_sim(
     Re_base10=Re_base10,
     Nref=Nref,
     density_jump=density_jump,
-    t_sim_approx=t_sim_approx,
+    target_times=target_times,
     tolerance=1e-5,
 )
 
@@ -85,7 +90,7 @@ def plot_fv(ax, sim):
     ax.set_ylim(0, 1.0)
 
     x_fv, y_fv, _ = sim.mesh.faces
-    z_fv = sim.snapshot_history[-1].w[5, :, :, 0].T
+    z_fv = superfv_to_uniform_cell_averaged_dye(sim, nout).T
     return ax.pcolormesh(cp.asnumpy(x_fv), cp.asnumpy(y_fv), z_fv)
 
 
@@ -95,7 +100,7 @@ def plot_sd(ax, sim):
 
     x_sd = sim.regular_faces()[0]
     y_sd = sim.regular_faces()[1]
-    z_sd = spd_to_uniform_cell_averaged_dye(sim)
+    z_sd = spd_to_uniform_cell_averaged_dye(sim, nout)
     return ax.pcolormesh(cp.asnumpy(x_sd), cp.asnumpy(y_sd), cp.asnumpy(z_sd))
 
 
@@ -120,4 +125,4 @@ if __name__ == "__main__":
     axs[1, 2].set_title(f"SD8 (N={NDOF})")
     plot_sd(axs[1, 2], spd8)
 
-    fig.savefig(f"sd-compare/lecoanet/t={t_sim_approx}.png", dpi=300, bbox_inches="tight")
+    fig.savefig(f"sd-compare/lecoanet/t={t_plot}.png", dpi=300, bbox_inches="tight")
